c6a75e140914ef77571379d0417ce55e9e063c95
EvaldasMencinskas <58887179+EvaldasMencinskas@users.noreply.github.com>
2020-04-29 12:59:53 +0300

    Initial commit

---
+++ questions/views.py
@@ -0,0 +1,78 @@
+import os
+import json
+from datetime import datetime
+import xlsxwriter
+from django.shortcuts import render, redirect
+from django.conf import settings
+
+results_dir = os.path.join(settings.MEDIA_ROOT, 'results')
+# Create media directory if it doesn't exist
+if not os.path.exists(settings.MEDIA_ROOT):
+    os.mkdir(settings.MEDIA_ROOT)
+# Create results directory within media directory if it doesn't exist
+if not os.path.exists(results_dir):
+    os.mkdir(results_dir)
+
+with open(os.path.join(settings.BASE_DIR, 'questions.json'), 'r', encoding='utf-8') as question_data:
+    questions = json.load(question_data)
+
+
+def show_questions(request):
+    return render(request, 'questions.html',
+                  context={
+                      'title': 'Testas',
+                      'questions': questions
+                  })
+
+
+def generate_file(request):
+    # Form is submitted via post method:
+    if request.method == 'POST':
+        file_name = '{}-results-{}.xlsx'.format(
+            request.POST['username'],
+            datetime.now().strftime("%Y%m%d_%H%M%S"))
+        file_path = os.path.join(results_dir, file_name)
+
+        answers = []
+        for foo in range(len(questions)):
+            answers.append([
+                int(request.POST.get('q_{}_{}'.format(foo, bar)))
+                for bar in range(len(questions[foo].get('quiz_questions')))
+            ])
+
+        # Define worksheet and workbook:
+        workbook = xlsxwriter.Workbook(file_path)
+        worksheet = workbook.add_worksheet()
+        bold = workbook.add_format({'bold': True})
+        col = 0
+        for i in range(len(questions)):
+            worksheet.write(0, col + i, 'Klausimas', bold)
+            worksheet.write(0, col + i + 1, 'ats', bold)
+            worksheet.set_column(0, 0, 16)
+            worksheet.set_column(2, 2, 15)
+            worksheet.set_column(4, 4, 14)
+            worksheet.set_column(1, 1, 3)
+            worksheet.set_column(3, 3, 3)
+            worksheet.set_column(5, 5, 3)
+            for j in range(len(answers[i])):
+                worksheet.write(j + 1, col + i, questions[i].get('quiz_questions')[j])
+                worksheet.write_number(j + 1, col + i + 1, answers[i][j])
+            col += 1
+
+        workbook.close()
+
+        return render(request, 'results.html',
+                      context={
+                          'title': 'Quiz results',
+                          'file': file_name
+                      })
+    # If url is accessed without post data, redirect to questions page:
+    return redirect('questions')
+
+
+def show_files(request):
+    return render(request, 'files.html',
+                  context={
+                      'title': 'Results',
+                      'files': os.listdir(results_dir)
+                  })
